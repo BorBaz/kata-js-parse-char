9795846f9fe02b4b625cbf939f9688241143001d
Borja <boorjabs@gmail.com>
2022-10-14 23:12:54 +0200

    Forma interesante de obtener un entero en un string en js

--- Kata.js
+++ Kata.js
@@ -2,4 +2,9 @@ function getAge(inputString){
     return Number(inputString.substring(0,1))
 }
 
+// Una forma mucho mas interesante de hacerlo es con parseInt, ya que solo devolvera el entero
+function getAge(inputString){
+    return parseInt(inputString);
+}
+
 console.log(getAge("4 years old"))
